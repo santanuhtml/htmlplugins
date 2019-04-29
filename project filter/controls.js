c2c175674c8c1d83fba888819727483d2401c5fd
santanu chowdhury <40321346+santanuhtml@users.noreply.github.com>
2019-04-29 13:50:04 +0530

    Add files via upload

---
+++ project filter/controls.js
@@ -0,0 +1,7 @@
+$(function() {
+    //Simple filter controls
+    $('.simplefilter li').click(function() {
+        $('.simplefilter li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
